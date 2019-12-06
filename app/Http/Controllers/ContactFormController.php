5361520cf69ac68f08d7034b4585018aad0fdb1b
Lubomir Mitana <lubomir.mitana@gmail.com>
2019-12-06 15:42:59 +0100

    Added comments to the controller incl. TODO

--- app/Http/Controllers/ContactFormController.php
+++ app/Http/Controllers/ContactFormController.php
@@ -3,6 +3,13 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 class ContactFormController extends Controller
 {
+    /**
+     * Submit form function placeholder TODO request should be stored in DB also
+     *
+     * @param Request $request
+     * @return \Illuminate\Http\JsonResponse
+     * @throws \Illuminate\Validation\ValidationException
+     */
     public function submit(Request $request) {
         $this->validate($request, [
             'name' => 'required|string',
