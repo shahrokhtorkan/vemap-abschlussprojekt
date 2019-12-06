8b94e49bcf28810262fb6a9880ca5f32f52f6635
Lubomir Mitana <lubomir.mitana@gmail.com>
2019-12-06 14:21:59 +0100

    Fixed error related to Assistant.php

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -50,7 +50,7 @@ class RegisterController extends Controller
     {
         return Validator::make($data, [
             'name' => ['required', 'string', 'max:255'],
-            'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
+            'email' => ['required', 'string', 'email', 'max:255', 'unique:assistants'],
             'password' => ['required', 'string', 'min:8', 'confirmed'],
         ]);
     }
