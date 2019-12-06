930c8d9ef832aef1a78a9131e514088a79ada746
shahrokhtorkan <torkan.shahrokh@gmail.com>
2019-12-06 23:21:17 +0100

    Responsive Contact Form

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -8,17 +8,16 @@
 </div>
 
 <div id="app">
-    <div class="flex-center position-ref full-height mb-5">
-        <div class="col-4 mx-auto">
-            <div class="card">
-                <div class="card-header">
-                    <h2>Contact us</h2>
-                </div>
-                <div class="card-body">
-                    <!--
-                        Our component:
-                    -->
-                    <contact-form></contact-form>
+    <div class="container mb-5">
+        <div class="row justify-content-center">
+            <div class="col-md-7">
+                <div class="card">
+                    <div class="card-header text-dark">{{ __('Contact Us') }}</div>
+                    <div class="card-body">
+                        <div class="card-body">
+                            <contact-form></contact-form>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
