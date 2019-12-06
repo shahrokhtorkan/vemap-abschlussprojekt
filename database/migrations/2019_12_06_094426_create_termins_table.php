ab26e6d47d9cd5516d851029d20c32aa692d418f
Lubomir Mitana <lubomir.mitana@gmail.com>
2019-12-06 10:46:39 +0100

    Added necessary models with migrations

---
+++ database/migrations/2019_12_06_094426_create_termins_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTerminsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('termins', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('termins');
+    }
+}
