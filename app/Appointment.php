863457210353b10b978c242377f58ba712394bbc
Lubomir Mitana <lubomir.mitana@gmail.com>
2019-12-06 11:13:43 +0100

    Added redirect to backend after register (no route yet)

--- app/Appointment.php
+++ app/Appointment.php
@@ -4,7 +4,7 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Dokument extends Model
+class Appointment extends Model
 {
     //
 }
--- database/migrations/2019_12_06_094409_create_documents_table.php
+++ database/migrations/2019_12_06_094409_create_documents_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateDokumentsTable extends Migration
+class CreateDocumentsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class CreateDokumentsTable extends Migration
      */
     public function up()
     {
-        Schema::create('dokuments', function (Blueprint $table) {
+        Schema::create('documents', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->timestamps();
         });
@@ -26,6 +26,6 @@ class CreateDokumentsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('dokuments');
+        Schema::dropIfExists('documents');
     }
 }
