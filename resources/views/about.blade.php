dbe56a5863ace8389197ed0c74598148e70fac11
shahrokhtorkan <torkan.shahrokh@gmail.com>
2019-12-07 20:36:23 +0100

    Add new info for about, imprint page!

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -15,7 +15,10 @@
                     <div class="card-header text-dark">{{ __('Über uns') }}</div>
                     <div class="card-body">
                         <div class="card-body">
-                            <about-us></about-us>
+                            <P>Anna Fink</P>
+                            <p>Als Physiotherapeutin ist mir die ganzheitliche Arbeit mit dem Patienten wichtig. Die Osteopathie biete ich ebenfalls an - sie ergänzt die Behandlung optimal. Barbara und ich arbeiten schon lange zusammen und wir freuen uns, auch Sie in unserer Praxis begrüßen zu dürfen.</P>
+                            <p>Barbara Berg</P>
+                            <P>Ich kenne Anna schon seit der Schulzeit und wir haben Teile unserer Ausbildungen gemeinsam gemacht. Gemeinsam möchten wir unseren Patientinnen und Patienten stets das Allerbeste bieten und das gelingt uns jeden tag aufs Neue.</p>
                         </div>
                     </div>
                 </div>
--- resources/views/imprint.blade.php
+++ resources/views/imprint.blade.php
@@ -15,7 +15,11 @@
                     <div class="card-header text-dark">{{ __('Impressum') }}</div>
                     <div class="card-body">
                         <div class="card-body">
-                            <imprint></imprint>
+                            <h2>Patient</h2>
+                            <p>Unsere Sekretariatsöffnungszeiten sind:</P>
+                            <P>Montag - Freitag 8:00 - 11:00</P>
+                            <P>+43 / 123 123 123 123</P>
+                            <P>office@example.com</P>
                         </div>
                     </div>
                 </div>
