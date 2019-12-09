ed54d92e3a8c7c7381db09d280efbea679ebb8b6
Lubomir Mitana <lubomir.mitana@gmail.com>
2019-12-09 11:26:25 +0100

    Created migration for inquiries.

---
+++ database/migrations/2019_12_09_101441_create_inquiries_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateInquiriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('inquiries', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name');
+            $table->string('email');
+            $table->string('phone');
+            $table->string('service');
+            $table->text('questions');
+            $table->integer('status')->default(1)->unsigned();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('inquiries');
+    }
+}
