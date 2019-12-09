82f14fce3b6b313875547e0e3930c76bcb778778
Lubomir Mitana <lubomir.mitana@gmail.com>
2019-12-09 08:52:58 +0100

    Defined relationship in Patient model

--- app/Patient.php
+++ app/Patient.php
@@ -6,5 +6,19 @@ use Illuminate\Database\Eloquent\Model;
 
 class Patient extends Model
 {
-    //
+    /**
+     * Define relationship to Document
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function documents() {
+        return $this->hasMany('App\Documentation')->orderBy('id', 'desc')  ;
+    }
+
+    /**
+     * Define relationship to Appointment
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function slots() {
+        return $this->hasMany('App\Appointment');
+    }
 }
