f73ad3ab31299e19f564767cb9cd63c505bdc900
shahrokhtorkan <torkan.shahrokh@gmail.com>
2019-12-09 16:28:20 +0100

    Added Table for Patient.

--- resources/views/backend/patients.blade.php
+++ resources/views/backend/patients.blade.php
@@ -6,16 +6,31 @@
         <div class="title text-center text-dark display-1 mb-5">
             <h1>Übersicht - Patienten</h1>
         </div>
-
         @if (count($patients) > 0)
-            @foreach($patients as $patient)
-                {{ $patient -> svnr }}
-                {{ $patient -> lastname }} {{ $patient -> firstname }}
-                {{ $patient -> address }}
-                {{ $patient -> plz }}
-                {{ $patient -> city }}
-                {{ $patient -> country }}
+        <table id="table_id" class="display table table-bordered table-hover">
+            <thead>
+            <tr>
+                <th>SVNr</th>
+                <th>Name</th>
+                <th>Adresse</th>
+                <th>PLZ</th>
+                <th>Stadt</th>
+                <th>Land</th>
+            </tr>
+            </thead>
+            <tbody>
+            <tr>
+                @foreach($patients as $patient)
+                <td>{{ $patient -> svnr }}</td>
+                <td>{{ $patient -> lastname }} {{ $patient -> firstname }}</td>
+                <td>{{ $patient -> address }}</td>
+                <td>{{ $patient -> plz }}</td>
+                <td>{{ $patient -> city }}</td>
+                <td>{{ $patient -> country }}</td>
+            </tr>
+            </tbody>
             @endforeach
+        </table>
         @endif
     </div>
 </div>
