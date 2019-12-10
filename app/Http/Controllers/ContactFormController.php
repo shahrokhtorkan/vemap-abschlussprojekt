1721f1e4ae2f4a3c1a2b4fee9aed047fb56570ce
Adnan ALazaat <33832766+MhdAdnanALazaat@users.noreply.github.com>
2019-12-10 11:06:58 +0100

    Merge branch 'master' of C:\xampp\htdocs\final\vemap-abschlussprojekt with conflicts.

--- app/Http/Controllers/ContactFormController.php
+++ app/Http/Controllers/ContactFormController.php
@@ -11,12 +11,37 @@ class ContactFormController extends Controller
      * @return \Illuminate\Http\JsonResponse
      * @throws \Illuminate\Validation\ValidationException
      */
+
+    public function create()
+    {
+        return view('');
+    }
+
     public function submit(Request $request) {
-        $this->validate($request, [
-            'name' => 'required|string',
-            'email' => 'required|email',
-            'message' => 'required',
+        $request->validate([
+            'name' => 'required|alpha_dash|max:255',
+            'email' => 'email',
+            'phone'=>'required|numeric|digits:10',
+            'Leistungen'=>'required',
+            'q1'=>'required',
+            'q2'=>'required',
+            'q3'=>'required',
+            'q4'=>'required',
+            'q5'=>'required',
         ]);
+
+        $contact = new contact();
+        $contact->name = $request->name;
+        $contact->email = $request->email;
+        $contact->phone = $request->phone;
+        $contact->Leistungen = $request->Leistungen;
+        $contact->q1 = $request->q1;
+        $contact->q2 = $request->q2;
+        $contact->q3 = $request->q3;
+        $contact->q4 = $request->q4;
+        $contact->q5 = $request->q5;
+        $contact->save();
+        dd($contact->name);
         /*
           Add mail functionality here.
         */
--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -1,22 +1,20 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="flex-center position-ref full-height mt-5">
-    <div class="content">
-        {{--<div class="title text-center text-dark display-1 mb-5">
-            <h1>Kontakt</h1>
-        </div>--}}
-        <div id="app">
-            <div class="container mb-5">
-                <div class="row justify-content-center">
-                    <div class="col-md-7">
-                        <div class="card">
-                            <div class="card-header text-dark card-top"><i class="fas fa-envelope mr-1"></i>{{ __('Kontakt') }}</div>
-                            <div class="card-body">
-                                <div class="card-body">
-                                    <contact-form></contact-form>
-                                </div>
-                            </div>
+{{--<div class="flex-center position-ref full-height mt-5">
+    <div class="title text-center text-dark display-1 mb-5">
+        <h1>Kontakt</h1>
+    </div>
+</div>--}}
+<div id="app">
+    <div class="container mb-5">
+        <div class="row justify-content-center">
+            <div class="col-md-7">
+                <div class="card">
+                    <div class="card-header text-dark"><i class="fas fa-envelope mr-1"></i>{{ __('Kontakt') }}</div>
+                    <div class="card-body">
+                        <div class="card-body">
+                            <contact-form></contact-form>
                         </div>
                     </div>
                 </div>
