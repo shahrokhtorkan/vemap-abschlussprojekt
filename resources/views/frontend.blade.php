5e4e5981609b777cfb12bb91ce87aee5e56b82b5
shahrokhtorkan <torkan.shahrokh@gmail.com>
2019-12-12 11:27:01 +0100

    unnecessary text Deleted.

--- resources/views/frontend.blade.php
+++ resources/views/frontend.blade.php
@@ -10,27 +10,6 @@
                     <span class="wrap"></span>
                 </h2>
             </span>
-
-        <div>
-            <a class="btn btn-primary" data-toggle="collapse" href="#collapseExample" aria-expanded="false" aria-controls="collapseExample">
-                Link with href
-            </a>
-            <button class="btn btn-primary" type="button" data-toggle="collapse" data-target="#collapseExample"
-                    aria-expanded="false" aria-controls="collapseExample">
-                Button with data-target
-            </button>
-        </div>
-        <!-- / Collapse buttons -->
-
-        <!-- Collapsible element -->
-        <div class="collapse" id="collapseExample">
-            <div class="mt-3">
-                Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. Nihil anim
-                keffiyeh helvetica,
-                craft beer labore wes anderson cred nesciunt sapiente ea proident.
-            </div>
-        </div>
-
         <div class="container">
             <div class="row justify-content-center">
                 <div class="col-md-6">
