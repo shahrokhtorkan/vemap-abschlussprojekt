1ed9f39288ca946c982f9687f9f436dffae4738c
shahrokhtorkan <torkan.shahrokh@gmail.com>
2019-12-12 20:58:14 +0100

    Title to Meine Termin Changed.

--- resources/views/backend/appointments.blade.php
+++ resources/views/backend/appointments.blade.php
@@ -4,14 +4,14 @@
 <div class="flex-center position-ref full-height mt-5">
     <div class="content">
         {{--<div class="title text-center text-dark display-1 mb-5">
-            <h1>Termine</h1>
+            <h1>Meine Termine</h1>
         </div>--}}
         <div id="app">
             <div class="container mb-5">
                 <div class="row justify-content-center">
                     <div class="col-md-8 mb-5">
                         <div class="card">
-                            <div class="card-header text-dark card-top">{{ __('Termine') }}</div>
+                            <div class="card-header text-dark card-top">{{ __('Meine Termine') }}</div>
                             <div class="card-body">
 
                                 {{-- Slots where('status' is 'reserved') --}}
