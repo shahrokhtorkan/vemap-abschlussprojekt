ed81c0f7b25ceb931f2fd6aa2316b87f629febed
Lubomir Mitana <lubomir.mitana@gmail.com>
2019-12-12 11:51:46 +0100

    Added filesystem for Document uploads

--- config/filesystems.php
+++ config/filesystems.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'default' => env('FILESYSTEM_DRIVER', 'local'),
+    'default' => env('FILESYSTEM_DRIVER', 'documents'),
 
     /*
     |--------------------------------------------------------------------------
@@ -49,6 +49,13 @@ return [
         ],
 
         'public' => [
+            'driver' => 'local',
+            'root' => storage_path('app/public'),
+            'url' => env('APP_URL').'/storage',
+            'visibility' => 'public',
+        ],
+
+        'documents' => [
             'driver' => 'local',
             'root' => storage_path('app/public'),
             'url' => env('APP_URL').'/storage',
