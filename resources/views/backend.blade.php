6efa974a160674a39acd4b477acb99d1d70b5d00
Adnan ALazaat <33832766+MhdAdnanALazaat@users.noreply.github.com>
2019-12-13 11:21:15 +0100

    fixed inquiries form

--- resources/views/backend.blade.php
+++ resources/views/backend.blade.php
@@ -1,23 +1,23 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="flex-center position-ref full-height mt-5">
-    <div class="content">
-        {{--<div class="title text-center text-dark display-1 mb-5">
-            <h1>Instrumententafel</h1>
-        </div>--}}
-        <div class="container mb-5">
-            <div class="row justify-content-center">
-                <div class="col-md-8 mb-5">
-                    <div class="card">
-                        <div class="card-header text-dark card-top"><i class="fas fa-user-tie mr-1"></i>{{ auth()->user()->name }}'s Profil</div>
-                        <div class="card-body">
-                            @if (session('status'))
-                                <div class="alert alert-success" role="alert">
-                                    {{ session('status') }}
-                                </div>
-                            @endif
-                            <h3 class="mb-4">Willkommen {{ auth()->user()->name }}</h3>
+    <div class="flex-center position-ref full-height mt-5">
+        <div class="content">
+            {{--<div class="title text-center text-dark display-1 mb-5">
+                <h1>Instrumententafel</h1>
+            </div>--}}
+            <div class="container mb-5">
+                <div class="row justify-content-center">
+                    <div class="col-md-8 mb-5">
+                        <div class="card">
+                            <div class="card-header text-dark card-top"><i class="fas fa-user-tie mr-1"></i>{{ auth()->user()->name }}'s Profil</div>
+                            <div class="card-body">
+                                @if (session('status'))
+                                    <div class="alert alert-success" role="alert">
+                                        {{ session('status') }}
+                                    </div>
+                                @endif
+                                <h3 class="mb-4">Willkommen {{ auth()->user()->name }}</h3>
                                 <h5>Sie haben neue Kontaktanfragen</h5>
                                 @if(!empty($inquiries))
                                     <table class="table table-bordered table-hover table-sm table-responsive-sm mb-5">
@@ -40,19 +40,19 @@
                                                         @csrf
                                                         <input class="form-check-input" type="hidden" name="status" value="0" checked/>
 
-{{--                                                <input type="text" name="status" value="{{ $q->status }}">--}}
-                                                    <input type="submit"></form></td>
+                                                        {{--                                                <input type="text" name="status" value="{{ $q->status }}">--}}
+                                                        <input type="submit"></form></td>
                                             </tr>
                                         @endforeach
                                     </table>
                                 @else
                                     <p>Keine Kontakt vorhanden.</p>
                                 @endif
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
 @endsection
