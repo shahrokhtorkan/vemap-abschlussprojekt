8b9a8059bece53783d55d9352b97494c1898d664
shahrokhtorkan <torkan.shahrokh@gmail.com>
2019-12-16 12:52:28 +0100

    Added class for paragraph.

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -12,7 +12,7 @@
                             <div class="card-body">
                                 <p><img src="../images/about.png" class="img-fluid rounded mx-auto d-block" alt="Responsive image"></p>
                                 <p><img src="../images/doctor.png" class="img-fluid rounded mx-auto d-block float-right" alt="Responsive image"></p>
-                                <p>Richard Zauner<br>
+                                <p class="mt-5">Richard Zauner<br>
                                    Praktischer Arzt, Schwerpunkt: Schmerztherapie</p>
                                 <p>
                                     Herzlich Willkommen bei meiner Praxis für Allgemeinmedizin und ganzheitliche Schmerztherapie. Als Wahlarzt kann ich Ihnen genügend Zeit für das Arzt-Patienten-Gespräch, ausreichend Zeit für Untersuchungen und Befundbesprechungen, kurzfristige Termine, Hausbesuche und nahezu keine Wartezeiten bieten. Jeder Behandlung gehen eine umfangreiche Anamnese sowie ein ausführliches Beratungsgespräch voraus. Anschließend informiere ich Sie detailliert zu den einzelnen Therapieschritten.
