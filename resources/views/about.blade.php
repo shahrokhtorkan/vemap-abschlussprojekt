062732e834397243528322c8fbd62fd516f45c34
Lubomir Mitana <lubomir.mitana@gmail.com>
2019-12-16 11:48:05 +0100

    Info for doctor changed

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -11,8 +11,11 @@
                             <div class="card-header text-dark card-top"><i class="fas fa-user-md mr-1"></i>{{ __('Über uns') }}</div>
                             <div class="card-body">
                                 <p><img src="../images/about.png" class="img-fluid rounded mx-auto d-block" alt="Responsive image"></p>
-                                <P>Anna Fink</P>
-                                <p>Als Physiotherapeutin ist mir die ganzheitliche Arbeit mit dem Patienten wichtig. Die Osteopathie biete ich ebenfalls an - sie ergänzt die Behandlung optimal. Barbara und ich arbeiten schon lange zusammen und wir freuen uns, auch Sie in unserer Praxis begrüßen zu dürfen.</P>
+                                <p>Richard Zauner<br>
+                                   Praktischer Arzt, Schwerpunkt: Schmerztherapie</p>
+                                <p>
+                                    Herzlich Willkommen bei meiner Praxis für Allgemeinmedizin und ganzheitliche Schmerztherapie. Als Wahlarzt kann ich Ihnen genügend Zeit für das Arzt-Patienten-Gespräch, ausreichend Zeit für Untersuchungen und Befundbesprechungen, kurzfristige Termine, Hausbesuche und nahezu keine Wartezeiten bieten. Jeder Behandlung gehen eine umfangreiche Anamnese sowie ein ausführliches Beratungsgespräch voraus. Anschließend informiere ich Sie detailliert zu den einzelnen Therapieschritten.
+                                </p>
                             </div>
                         </div>
                     </div>
