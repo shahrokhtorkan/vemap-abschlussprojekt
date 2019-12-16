e229033b341bd11c86da64ce042168fd44a51feb
Lubomir Mitana <lubomir.mitana@gmail.com>
2019-12-16 16:14:26 +0100

    Improved typewriter text in frontend.blade.php.

--- resources/views/frontend.blade.php
+++ resources/views/frontend.blade.php
@@ -4,9 +4,9 @@
 <div class="flex-center position-ref full-height mt-5">
     <div class="content">
         <p><img src="../images/logo.png" class="img-fluid rounded mx-auto d-block" alt="Responsive image"></p>
-        <h2 class="text-center mt-5 mb-5">Treffen Sie unsere Fachärzte,<a href="{{ url('/register') }}"> HIER REGISTRIEREN!</a></h2>
+        <h2 class="text-center mt-5 mb-5">Nutzen Sie unser Anfrageformular <a href="{{ url('/contact') }}">hier</a> und schicken Sie uns eine Anfrage.</h2>
             <span>
-                <h2 class="typewrite text-center" data-period="2000" data-type='[ "Es ist KOSTENLOS für Sie.", "HAST DU LUST?", "Einfach registrieren und nutzen."]'>
+                <h2 class="typewrite text-center" data-period="2000" data-type='[ "Ihr Praktischer Arzt mit Schwerpunkt Schmerztherapie.", "Haben Sie Fragen?", "Senden Sie uns Ihre Anfrage mit Hilfe unseres Kontaktformulars."]'>
                     <span class="wrap"></span>
                 </h2>
             </span>
