784cd866b012cf0dc3f62f986eff1dfddd4f44e6
Lubomir Mitana <lubomir.mitana@gmail.com>
2019-12-16 14:36:31 +0100

    Email functionality implemented via provisory route

--- resources/views/emails/PatientDocumentNotificationEmail.blade.php
+++ resources/views/emails/PatientDocumentNotificationEmail.blade.php
@@ -1,1 +1,16 @@
-Your document was updated.
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <title>Wichtige Benachrichtigung</title>
+    </head>
+
+    <body>
+        <h1>Änderungen in Ihrem Konto</h1>
+        <p>Ein Dokument wurde zum Ihr Konto hochgeladen.</p>
+        <p>Mit freundlichen Grüßen</p>
+        <p>Dr. Richard Zauner<br>
+           +43/123 123 123 123<br>
+           ordination@example.com</p>
+    </body>
+</html>
