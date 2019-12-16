a7e351062af4ada8b0c6fb6eb9d334ec20a40b84
Lubomir Mitana <lubomir.mitana@gmail.com>
2019-12-16 11:56:19 +0100

    Edited services.blade.php

--- resources/views/services.blade.php
+++ resources/views/services.blade.php
@@ -11,19 +11,26 @@
                             <div class="card-header text-dark card-top"><i class="fas fa-toolbox mr-1"></i>{{ __('Leistungen') }}</div>
                             <div class="card-body">
                                 <p><img src="../images/services.png" class="img-fluid rounded mx-auto d-block" alt="Responsive image"></p>
-                                <p>Therapie Aktiv Betreuungsprogramm</p>
-                                <p>Marcoumar-Einstellung</p>
-                                <p>Schmerztherapie</p>
-                                <p>Nahtentfernung</p>
-                                <p>Verbandwechsel</p>
-                                {{--<p>Physiotherapie<P>
-                                <p>Sport-Physiotherapie</p>
-                                <p>Fußreflexzonenmassage</p>
-                                <p>Lymphdrainage</p>
-                                <p>Kinesio-Taping</p>
-                                <p>Osteopathie</p>
-                                <p>Fasziendistorsionsmodell (FDM)</p>
-                                <p>Craniosacral-Therapie</p>--}}
+                                <p>
+                                    Therapie Aktiv Betreuungsprogramm<br>
+                                    Der "Therapie Aktiv"-Arzt behandelt seine Patienten umfassend, kontrolliert regelmäßig die Füße, sorgt dafür, dass regelmäßig notwendige Blutparameter wie zB HbA1c bestimmt werden und veranlasst wichtige Untersuchungen wie zB jährliche Augenkontrollen.
+                                </p>
+                                <p>
+                                    Marcoumar-Einstellung<br>
+                                    Blutgerinnung ist für den Menschen überlebenswichtig. Bei einer Marcumartherapie wählen Ärzte deshalb den Mittelweg zwischen dem Schutz vor Blutgerinnseln und dem Blutungsrisiko durch Gerinnungshemmung. Die Einstellung der Dosis wird über die sogenannte Thromboplastinzeit (TPZ oder kurz „Quick-Test“) gesteuert. Der Arzt gibt sogenannten „Quick-Wert“, den der Test erreichen soll, vor.
+                                </p>
+                                <p>
+                                    Schmerztherapie<br>
+                                    Mit der Schmerztherapie werden akute und chronische Schmerzen behandelt sowie versucht die Ursache für das Auftreten der Schmerzen zu ergründen und zu beseitigen. Die Behandlung wird meistens von Schmerztherapeuten in Schmerzpraxen durchgeführt.
+                                </p>
+                                <p>
+                                    Nahtentfernung<br>
+                                    Die operative Entfernung von krebsverdächtigen Neubildungen, auffälligen Muttermalen und Probeentnahmen von unklaren Hautveränderungen führe ich ambulant in meiner Ordination durch.
+                                </p>
+                                <p>
+                                    Verbandwechsel<br>
+                                    Unter einem Verbandwechsel versteht man den Wechsel von Wundauflagen und Verbänden, mit denen eine Wunde versorgt wurde.
+                                </p>
                             </div>
                         </div>
                     </div>
