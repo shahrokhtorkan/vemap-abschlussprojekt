5d19c71087dcee815f5055bcf88f88f28b6c7187
shahrokhtorkan <torkan.shahrokh@gmail.com>
2019-12-16 11:45:18 +0100

    unnecessary service Commit.

--- resources/views/services.blade.php
+++ resources/views/services.blade.php
@@ -11,14 +11,19 @@
                             <div class="card-header text-dark card-top"><i class="fas fa-toolbox mr-1"></i>{{ __('Leistungen') }}</div>
                             <div class="card-body">
                                 <p><img src="../images/services.png" class="img-fluid rounded mx-auto d-block" alt="Responsive image"></p>
-                                <p>Physiotherapie<P>
+                                <p>Therapie Aktiv Betreuungsprogramm</p>
+                                <p>Marcoumar-Einstellung</p>
+                                <p>Schmerztherapie</p>
+                                <p>Nahtentfernung</p>
+                                <p>Verbandwechsel</p>
+                                {{--<p>Physiotherapie<P>
                                 <p>Sport-Physiotherapie</p>
                                 <p>Fußreflexzonenmassage</p>
                                 <p>Lymphdrainage</p>
                                 <p>Kinesio-Taping</p>
                                 <p>Osteopathie</p>
                                 <p>Fasziendistorsionsmodell (FDM)</p>
-                                <p>Craniosacral-Therapie</p>
+                                <p>Craniosacral-Therapie</p>--}}
                             </div>
                         </div>
                     </div>
