35b4042f52217a3b8ce902faeaf085837233dd11
shahrokhtorkan <torkan.shahrokh@gmail.com>
2019-12-16 14:36:11 +0100

    Class Changed.

--- resources/views/backend/patients.blade.php
+++ resources/views/backend/patients.blade.php
@@ -4,7 +4,7 @@
     <div class="flex-center position-ref full-height mt-4">
         <div class="content">
             <div id="app">
-                <div class="container mb-5">
+                <div class="container mb-4">
                     <div class="row justify-content-center">
                         <div class="col-md-12 mb-5">
                             <div class="card">
